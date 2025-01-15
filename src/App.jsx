6915c0abd3465e34f876d9a0cdfe2c1b4ae16bdf
AlexStraton <alexandra.straton@curaleafint.com>
2025-01-15 20:43:05 +0000

    created forlder structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const API_KEY = '87c47b251a9f5c499a515bcc5300d0815f11b82100e17282';
+const BASE_URL = 'https://jse-assignment.uk';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Hello
+      </div>
+
+    </>
+  )
+}
+
+export default App
